1038d2a2984cdda97d2d0abb5c28f3263e4908f9
Lara Aguilar <72893552+laraguilar@users.noreply.github.com>
2024-12-19 10:17:00 -0300

    config/configure_eslint_prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'semi': ['error', 'always'], // Exige ponto e vírgula
+      'quotes': ['error', 'single'], // Exige aspas simples
+      'no-unused-vars': 'warn', // Avisa sobre variáveis não usadas
+      'react/react-in-jsx-scope': 'off', // Desativa a regra para importar React no JSX 
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      // '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      // '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-empty-interface': 'warn',
+      // 'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    }
+  }
 ];
-
-export default eslintConfig;
